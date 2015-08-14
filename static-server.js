5cee845d2dbe4b3dfbe0402b7d30472d2da0ba52
Nelson <contact.nelsonic@gmail.com>
2015-08-14 23:37:16 +0100

    adds devDependency on inert plugin for serving static files. see: https://github.com/hapijs/hapi/issues/2682#issuecomment-130574238 fixes https://github.com/dwyl/learn-tdd/issues/22

--- static-server.js
+++ static-server.js
@@ -1,4 +1,9 @@
-var Hapi = require('hapi'), Path = require('path'), server = new Hapi.Server(), port = process.env.PORT || 8000;
+var Hapi = require('hapi'), Inert = require('inert'), Path = require('path'),
+server = new Hapi.Server(), port = process.env.PORT || 8000;
+server.register(Inert, function () {});
 server.connection({ port: port });
-server.route( { method: 'GET', path: '/{param*}', handler: { directory: { path: Path.normalize(__dirname + '/') } } });
-server.start(function() { console.log('Static Server Listening on : http://127.0.0.1:' +port) });
+server.route( { method: 'GET', path: '/{param*}', handler: {
+    directory: { path: Path.normalize(__dirname + '/') }
+  }
+});
+server.start(function() { console.log('Visit: http://127.0.0.1:' +port) });
