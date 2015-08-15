22a8cb245384a824c83f9766642e643bad89e622
Nelson <contact.nelsonic@gmail.com>
2015-08-15 11:11:57 +0100

    tidy static-server for readability (beginner-friendly)

--- static-server.js
+++ static-server.js
@@ -1,9 +1,16 @@
-var Hapi = require('hapi'), Inert = require('inert'), Path = require('path'),
-server = new Hapi.Server(), port = process.env.PORT || 8000;
-server.register(Inert, function () {});
-server.connection({ port: port });
-server.route( { method: 'GET', path: '/{param*}', handler: {
-    directory: { path: Path.normalize(__dirname + '/') }
-  }
-});
-server.start(function() { console.log('Visit: http://127.0.0.1:' +port) });
+var Hapi   = require('hapi');
+var Inert  = require('inert'); // serve static content
+var Path   = require('path');
+var server = new Hapi.Server();
+var port   = process.env.PORT || 8000;
+server.register(Inert, function () {
+  server.connection({ port: port });
+  server.route( {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: { path: Path.normalize(__dirname + '/') }
+    }
+  });
+  server.start(function() { console.log('Visit: http://127.0.0.1:' +port) });
+}); // requires a callback function but can be blank
