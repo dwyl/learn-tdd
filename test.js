a6de34115af22bee1ad6fb4b62eba74dcdc371a2
Valter Lima Mesquita <limavalter@gmail.com>
2016-04-01 12:09:25 -0300

    Fixed typo 2

--- test.js
+++ test.js
@@ -13,7 +13,7 @@ test('getChange(215, 300) should return [50, 20, 10, 5]', function(assert) {
   assert.deepEqual(result, expected);
 });
 
-test('getChange(486, 500) should equal [100, 10, 2, 2]', function(assert) {
+test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
   var result = getChange(486, 600);
   var expected = [100, 10, 2, 2];
   assert.deepEqual(result, expected);
